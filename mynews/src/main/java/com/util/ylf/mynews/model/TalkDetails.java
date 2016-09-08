bf1cd5b8f0c87dcc3cf45b1496c8838efff693a9
langfei yang <185601452@qq.com>
2016-09-08 15:37:52 +0800

    video_talkdetail

---
+++ mynews/src/main/java/com/util/ylf/mynews/model/TalkDetails.java
@@ -0,0 +1,7 @@
+package com.util.ylf.mynews.model;
+
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+public class TalkDetails {
+}
---
+++ mynews/src/main/java/com/util/ylf/mynews/talkmsg/TalkAdapter.java
@@ -0,0 +1,7 @@
+package com.util.ylf.mynews.talkmsg;
+
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+public class TalkAdapter {
+}
