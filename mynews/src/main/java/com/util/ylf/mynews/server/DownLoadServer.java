7369217703b2c8f2c7d874308607bd1d430c33fa
langfei yang <185601452@qq.com>
2016-09-09 18:06:45 +0800

    server   downLoad

--- mynews/src/main/java/com/util/ylf/mynews/server/DownLoadServer.java
+++ mynews/src/main/java/com/util/ylf/mynews/server/DownLoadServer.java
@@ -6,11 +6,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.database.Cursor;
 import android.net.Uri;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * Created by Administrator on 2016/9/9.
@@ -27,31 +31,56 @@ public class DownLoadServer extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        uploadApk("http://imtt.dd.qq.com/16891/49D0FEB62F426DBA907480A8B9B6A058.apk?fsname=com.example.androidnetwork_2.6_16.apk&csr=4d5s");
+        uploadApk("http://p3.so.qhmsg.com/bdr/326__/t0184315399f3f5fab8.jpg", "jcp");
     }
 
-    public void uploadApk(String url) {
+    public void uploadApk(String url, final String versionName) {
         final DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 
         final DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        final long id = manager.enqueue(request);
         //设置下载任务需要的网络状态
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
+        //设置通知栏信息
         request.setTitle("downLoad complete");
         request.setDescription("下载完成,哈哈哈");
-        //设置下载文件保存路径
-        //  request.setDestinationUri(Uri.parse(""));
+        //通知栏是否可见
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        //设置下载文件类型   可以在下载后自动打开
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        String type = mimeTypeMap.getMimeTypeFromExtension(url);
+        request.setMimeType(type);
 
+        //设置下载文件保存路径
+        request.setDestinationInExternalPublicDir("/dowmload", versionName);
+        //开始下载    返回唯一id可以进行断点  下载 和获取任务状态
+        final long id = manager.enqueue(request);
 
+        //监听任务下载完成  和 任务点击
         IntentFilter filter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
-
-
+        filter.addAction(DownloadManager.ACTION_NOTIFICATION_CLICKED);
         //注册下载完成的广播通知
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 long reference = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+                if (intent.getAction().equals(DownloadManager.ACTION_DOWNLOAD_COMPLETE)) {
+                    Toast.makeText(DownLoadServer.this, "下载完成", Toast.LENGTH_SHORT).show();
+                    try {
+                        manager.openDownloadedFile(reference);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    String downloadPath = Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + File.separator + versionName;
+                    installAPK(new File(downloadPath));
+                }
+
+                if (intent.getAction().equals(DownloadManager.ACTION_NOTIFICATION_CLICKED)) {
+                    Toast.makeText(DownLoadServer.this, "click notify", Toast.LENGTH_SHORT).show();
+                }
+
+             /*   //获取下载任务信息
                 if (id == reference) {
                     DownloadManager.Query myDownloadQuery = new DownloadManager.Query();
                     myDownloadQuery.setFilterById(reference);
@@ -64,21 +93,48 @@ public class DownLoadServer extends Service {
                         int fileUriIdx =
                                 myDownload.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI);
                         //下载失败原因
-                        int reason = myDownload.getColumnIndex(DownloadManager.COLUMN_REASON);
+                        int downReason = myDownload.getColumnIndex(DownloadManager.COLUMN_REASON);
+                        int downState = myDownload.getColumnIndex(DownloadManager.COLUMN_STATUS);
 
                         String fileName = myDownload.getString(fileNameIdx);
                         String fileUri = myDownload.getString(fileUriIdx);
+                        //获取下载状态
+                        //    DownloadManager.STATUS_SUCCESSFUL     成功
+                        //    DownloadManager.STATUS_FAILED          失败
+                        //    DownloadManager.STATUS_PAUSED         暂停
+                        //    DownloadManager.STATUS_RUNNING        正在下载
+                        //    DownloadManager.STATUS_PENDING        下载延迟
+                        int state = myDownload.getInt(downState);
+                        //下载失败原因
+                        int resaon = myDownload.getInt(downReason);
 
                         // TODO Do something with the file.
                         Log.d("111", fileName + " : " + fileUri);
                     }
                     myDownload.close();
-                }
+                }*/
+
+
             }
         };
         registerReceiver(receiver, filter);
 
 
+    }
+
+    private void installAPK(File file) {
+        //安装apk
+        if (!file.exists()) {
+            return;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = Uri.parse("file://" + file.toString());
+        intent.setDataAndType(uri, "application/vnd.android.package-archive");
+        //在服务中开启activity必须设置flag
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        //任务结束   销毁自己
+        stopSelf();
     }
 
     @Override
